d450ee209b736cfe4a8c8e36125fe318c8062f9b
alisaidgunes <alisaidgunes1@gmail.com>
2023-07-19 02:39:10 +0300

    add: helmet and cors middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import helmet from 'helmet';
+import {AppModule} from './app.module';
 import configuration from "./config/configuration";
 import {CustomResponseInterceptor} from "./interceptors/CustomResponseInterceptor";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new CustomResponseInterceptor());
-  await app.listen(configuration().port);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.use(helmet());
+    app.useGlobalInterceptors(new CustomResponseInterceptor());
+    await app.listen(configuration().port);
 }
+
 bootstrap();
