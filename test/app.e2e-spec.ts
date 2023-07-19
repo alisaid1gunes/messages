b8a6e6ec919ba8a0b282819ea32c938ed85e840f
alisaidgunes <alisaidgunes1@gmail.com>
2023-07-19 20:08:46 +0300

    create e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { RoomsService } from './../src/rooms/rooms.service';
+import {RoomDTO} from "../src/rooms/dto/room.dto";
+
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let roomsService: RoomsService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +16,43 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    roomsService = moduleFixture.get<RoomsService>(RoomsService);
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/rooms (GET)', async () => {
+    // Mock the getRooms method of the RoomsService
+    const mockResult = Promise.resolve({
+      rooms: [
+        {
+          id: 1,
+          lastMessage: {
+            content: 'Message 1',
+            createdAt: new Date().toISOString(), // Convert Date to string
+            receiver: { fullName: 'Receiver 1', photo: 'receiver-photo-url', id: 1 },
+            sender: { fullName: 'Sender 1', photo: 'sender-photo-url', id: 2 },
+          },
+        },
+        {
+          id: 2,
+          lastMessage: {
+            content: 'Message 2',
+            createdAt: new Date().toISOString(), // Convert Date to string
+            receiver: { fullName: 'Receiver 2', photo: 'receiver-photo-url', id: 3 },
+            sender: { fullName: 'Sender 2', photo: 'sender-photo-url', id: 4 },
+          },
+        },
+      ],
+    });
+
+    jest.spyOn(roomsService, 'getRooms').mockReturnValue(mockResult);
+
+    const response = await request(app.getHttpServer()).get('/rooms');
+
+    expect(response.status).toBe(HttpStatus.OK);
+    expect(response.body).toEqual({
+      result: await mockResult,
+      message: 'All rooms found',
+    });
   });
 });
